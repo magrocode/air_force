bf2b6d19fe6052e261729f1e4ed5e592fd5233c8
Mario Espinoza <mario@magrocode.com>
2012-02-09 17:33:01 -0300

    primer commit: esqueleto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirForce::Application.config.secret_token = '204a7230eab951ce09949b2345463ac14d05ef285cfa86ddae2c6056cfae01442f22730a75dd3b1eb082d2836bc07d93f0a8bff4f1cbed9b346fb09999da50bb'
